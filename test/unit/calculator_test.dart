626b2077e7a7760f46d7abce2c4126ca3fb156ca
Donkevin <kevinohiro@gmail.com>
2021-02-19 22:29:59 +0000

    using the setup func

--- test/unit/calculator_test.dart
+++ test/unit/calculator_test.dart
@@ -3,5 +3,13 @@ import 'package:testapp/calculator.dart';
 
 
 void main(){
-  
+
+  Calculator calculator;
+
+  setUp((){
+    calculator = Calculator();
+  });
+  test('adding 2 and 2 returns 4', () {
+    expect(calculator.addition(2, 2), 4);
+  });
 }
