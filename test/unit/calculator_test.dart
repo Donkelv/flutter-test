9f3aa1faa7baf25b9363846b1b02c64ff6ba6cc7
Donkevin <kevinohiro@gmail.com>
2021-02-19 22:33:31 +0000

    grouped the test

--- test/unit/calculator_test.dart
+++ test/unit/calculator_test.dart
@@ -9,7 +9,16 @@ void main(){
   setUp((){
     calculator = Calculator();
   });
-  test('adding 2 and 2 returns 4', () {
+  
+  group('Addition tests', (){
+    test('adding 2 and 2 returns 4', () {
     expect(calculator.addition(2, 2), 4);
+  });
+  
+
+  test('adding two negative numbers -2 and -2 gives a positive number 4', () {
+    expect(calculator.addition(-2, -2), -4);
+  });
+
   });
 }
