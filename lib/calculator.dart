9ab4feaf482d1ffcdcc005a9f43c1aad5524541f
Donkevin <kevinohiro@gmail.com>
2021-02-19 22:21:26 +0000

    calculator class added

---
+++ lib/calculator.dart
@@ -0,0 +1,9 @@
+class Calculator{
+  double addition(double a, double b) => a + b;
+
+  double subtraction(double a, double b) => a - b;
+
+  double division(double a, double b) => a / b;
+
+  double multiplication(double a , double b) => a * b;
+}
